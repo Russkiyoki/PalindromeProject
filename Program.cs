71aa4c16e8369995f82b1b21e386280bab041c8c
elivo <elivoich@gmail.com>
2023-12-06 18:17:47 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Project
+{
+    //project for CSE101 Clark College
+    //checks if input is the same reversed
+    public class Program
+    {
+        static public bool isPali;
+        public static string input;
+        static void Main(string[] args)
+        {
+            input = Console.ReadLine();
+            bool isPali = IsPali(input);
+            Console.WriteLine(isPali);
+        }
+        static public bool IsPali(string inp)
+        {
+            char[] charArray = inp.ToCharArray();
+            Array.Reverse(charArray);
+            string reversedInput = new string(charArray);
+
+            if(inp == reversedInput)
+            {
+                isPali = true;
+            }
+            else
+            {
+                isPali = false;
+            }
+            //Console.WriteLine(isPali);
+
+            return isPali;
+        }
+    }
+}
